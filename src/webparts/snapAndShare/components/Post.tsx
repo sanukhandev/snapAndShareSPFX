c2356d3847818daa8cc6e8012d2a62408824a5c8
Rakshitha <rakshithachandran98@gmail.com>
2025-03-14 10:11:05 +0400

    Refactor Post component to manage likes state locally, update like handling logic, and improve modal image selection

--- src/webparts/snapAndShare/components/Post.tsx
+++ src/webparts/snapAndShare/components/Post.tsx
@@ -1,3 +1,162 @@
+// import * as React from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faHeart } from "@fortawesome/free-solid-svg-icons";
+// import Modal from "./Modal";
+
+// interface IPost {
+//   id: number;
+//   title: string;
+//   postedBy: string;
+//   postedByEmail: string;
+//   postedByRole: string;
+//   likes: string;
+//   images: string[];
+//   comments: IComment[];
+// }
+// interface IComment {
+//   id: number;
+//   comment: string;
+//   postedBy: string;
+//   likes?: string;
+// }
+
+// interface PostProps {
+//   post: IPost;
+//   userId: number;
+//   likedPosts: Set<number>;
+//   likedComments: Set<number>;
+//   onLikePost: (postId: number) => void;
+//   // onLikeComment: (commentId: number) => void;
+//   onAddComment: (postId: number, comment: string) => void;
+// }
+
+// interface PostState {
+//   isModalOpen: boolean;
+//   selectedImage: number;
+//   newComment: string;
+// }
+
+// class Post extends React.Component<PostProps, PostState> {
+//   constructor(props: PostProps) {
+//     super(props);
+//     this.state = {
+//       isModalOpen: false,
+//       selectedImage: 0,
+//       newComment: "",
+//     };
+//   }
+
+//   openModal = (index: number): void => {
+//     this.setState({ selectedImage: index, isModalOpen: true });
+//   };
+
+//   handleCommentSubmit = (): void => {
+//     if (this.state.newComment.trim()) {
+//       this.props.onAddComment(this.props.post.id, this.state.newComment);
+//       this.setState({ newComment: "" });
+//     }
+//   };
+
+//   render(): JSX.Element {
+//     const { post, onLikePost } = this.props;
+//     return (
+//       <div className="post-container border p-4 mb-5 rounded-3">
+//         <h3>{post.title}</h3>
+//         <p className="text-muted text-base small">
+//           By <span className="text-black">{post.postedBy}</span>
+//         </p>
+
+//         <div className="row">
+//           {post.images.slice(0, 2).map((img, index) => (
+//             <div key={index} className="col-4">
+//               <div
+//                 className="w-100 position-relative rounded-4"
+//                 onClick={() => this.openModal(index)}
+//               >
+//                 <img
+//                   className="img-thumbnail w-100 rounded-4 p-0"
+//                   src={img}
+//                   alt="Post"
+//                 />
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+
+//         <div className="d-flex align-items-center my-4">
+//           <button
+//             className="btn btn-secondary d-inline-flex align-items-center rounded-5 text-white px-3 py-1 fw-bold"
+//             onClick={() => onLikePost(post.id)}
+//           >
+//             <FontAwesomeIcon
+//               icon={faHeart}
+//               className={
+//                 post.likes.split(";").includes(this.props.userId.toString())
+//                   ? "text-red-500"
+//                   : "text-gray-500"
+//               }
+//             />
+//             {post.likes.split(";").length}
+//           </button>
+//         </div>
+
+//         <div className="mt-2 d-flex">
+//           <input
+//             type="text"
+//             className="form-control comment-box"
+//             placeholder="Add your comment"
+//             value={this.state.newComment}
+//             onChange={(e) => this.setState({ newComment: e.target.value })}
+//           />
+//           <button
+//             className="btn btn-danger ms-2 btn btn-primary text-white ms-4"
+//             onClick={this.handleCommentSubmit}
+//           >
+//             Comment
+//           </button>
+//         </div>
+
+//         <div className="mt-4">
+//           {post.comments.map((comment) => (
+//             <div key={comment.id} className="d-flex align-items-start mb-3">
+//               <div
+//                 className="user-avatar bg-danger d-flex align-items-center justify-content-center me-3 p-2 rounded-circle text-white"
+//                 style={{ width: "40px", height: "40px" }}
+//               >
+//                 {comment.postedBy
+//                   .split(" ")
+//                   .map((name) => name[0])
+//                   .join("")
+//                   .toUpperCase()
+//                   .substring(0, 2)}
+//               </div>
+//               <div className="flex-grow-1">
+//                 <h6 className="mb-0">
+//                   <strong>{comment.postedBy}</strong>
+//                   <span className="text-muted small"> Feb 28 2025</span>
+//                 </h6>
+//                 <p className="mb-1">{comment.comment}</p>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+
+//         {this.state.isModalOpen && (
+//           <Modal
+//             title={post.title}
+//             onClose={() => this.setState({ isModalOpen: false })}
+//             images={post.images}
+//             initialIndex={this.state.selectedImage}
+//           />
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+// export default Post;
+
+
 import * as React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
@@ -25,8 +184,7 @@ interface PostProps {
   userId: number;
   likedPosts: Set<number>;
   likedComments: Set<number>;
-  onLikePost: (postId: number) => void;
-  // onLikeComment: (commentId: number) => void;
+  onLikePost: (postId: number, updatedLikes: string) => void;
   onAddComment: (postId: number, comment: string) => void;
 }
 
@@ -34,20 +192,41 @@ interface PostState {
   isModalOpen: boolean;
   selectedImage: number;
   newComment: string;
+  likes: string[];
+  isLiked: boolean;
 }
 
 class Post extends React.Component<PostProps, PostState> {
   constructor(props: PostProps) {
     super(props);
+    const initialLikes = props.post.likes ? props.post.likes.split(";") : [];
     this.state = {
       isModalOpen: false,
       selectedImage: 0,
       newComment: "",
+      likes: initialLikes,
+      isLiked: initialLikes.includes(props.userId.toString()),
     };
   }
 
-  openModal = (index: number): void => {
-    this.setState({ selectedImage: index, isModalOpen: true });
+  handleLike = (): void => {
+    this.setState((prevState) => {
+      let updatedLikes;
+      let isLiked = !prevState.isLiked;
+      
+      if (isLiked) {
+        updatedLikes = [...prevState.likes, this.props.userId.toString()];
+      } else {
+        updatedLikes = prevState.likes.filter((id) => id !== this.props.userId.toString());
+      }
+      
+      this.props.onLikePost(this.props.post.id, updatedLikes.join(";"));
+      
+      return {
+        likes: updatedLikes,
+        isLiked: isLiked,
+      };
+    });
   };
 
   handleCommentSubmit = (): void => {
@@ -58,7 +237,7 @@ class Post extends React.Component<PostProps, PostState> {
   };
 
   render(): JSX.Element {
-    const { post, onLikePost } = this.props;
+    const { post } = this.props;
     return (
       <div className="post-container border p-4 mb-5 rounded-3">
         <h3>{post.title}</h3>
@@ -71,7 +250,7 @@ class Post extends React.Component<PostProps, PostState> {
             <div key={index} className="col-4">
               <div
                 className="w-100 position-relative rounded-4"
-                onClick={() => this.openModal(index)}
+                onClick={() => this.setState({ selectedImage: index, isModalOpen: true })}
               >
                 <img
                   className="img-thumbnail w-100 rounded-4 p-0"
@@ -86,17 +265,13 @@ class Post extends React.Component<PostProps, PostState> {
         <div className="d-flex align-items-center my-4">
           <button
             className="btn btn-secondary d-inline-flex align-items-center rounded-5 text-white px-3 py-1 fw-bold"
-            onClick={() => onLikePost(post.id)}
+            onClick={this.handleLike}
           >
             <FontAwesomeIcon
               icon={faHeart}
-              className={
-                post.likes.split(";").includes(this.props.userId.toString())
-                  ? "text-red-500"
-                  : "text-gray-500"
-              }
+              className={this.state.isLiked ? "text-danger" : "text-gray-500"}
             />
-            {post.likes.split(";").length}
+            <span className="ms-2">{this.state.likes.length}</span>
           </button>
         </div>
 
